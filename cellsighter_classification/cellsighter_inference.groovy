d36a652f25bcdf6e72e6835cee4ea040f0168760
juliajessika <52280791+juliajessika@users.noreply.github.com>
2026-05-04 13:48:16 +0300

    Update cellsighter_inference.groovy

--- cellsighter_classification/cellsighter_inference.groovy
+++ cellsighter_classification/cellsighter_inference.groovy
@@ -355,7 +355,7 @@ def optionalString = { def value ->
 // Load YAML config
 // ============================================================
 
-def configFile = new File(buildFilePath(PROJECT_BASE_DIR, "cell_sighter_inference_config.yaml"))
+def configFile = new File(buildFilePath(PROJECT_BASE_DIR, "cellsighter_inference.yaml"))
 
 if (!configFile.exists()) {
     println "ERROR: YAML config not found:"
